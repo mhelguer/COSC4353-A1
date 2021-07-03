b8a22a36dc5630bea41849c1e0657a48a8ee5b75
jubg11 <70234036+jubg11@users.noreply.github.com>
2021-07-02 19:20:47 -0500

    Add files via upload

---
+++ assignment2/config/db_connect.php
@@ -0,0 +1,18 @@
+<?php 
+	/* NEW:
+	// Database credentials. Assuming running MySQL
+	define('DB_SERVER', "project-management-3380.ctkzz4wlfaku.us-east-2.rds.amazonaws.com");
+	define('DB_USERNAME', "admin");
+	define('DB_PASSWORD', "password");
+	define('DB_NAME', "project management");
+
+	/* Attempt to connect to MySQL database 
+	$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+	 
+	// Check connection
+	if($link === false){
+	    die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
+	*/
+
+?>
